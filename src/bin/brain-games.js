6643b5ac7e08fe92675bcf7b060b33a148eb0461
troyanec <sergey-vyborov@yandex.ru>
2018-10-02 17:22:11 +0300

    Исправление замечаний

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!\n");
-
 import AskUserName from '..';
+
+console.log("Welcome to the Brain Games!\n");
 
 AskUserName();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 export default () => {
-  const UserName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ', UserName, '!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hello, ', userName, '!');
 };
